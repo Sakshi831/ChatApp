b1a347a2620374a4f76259152b4e0a79001ff4aa
Sakshi Randhave <sakshirandhave07@gmail.com>
2024-10-28 17:36:21 -1200

    Bottom Navigation & Fragment

--- app/src/main/java/com/example/chatapp/HomeActivity.java
+++ app/src/main/java/com/example/chatapp/HomeActivity.java
@@ -1,14 +1,56 @@
 package com.example.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView btnNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btnNav=findViewById(R.id.bottomNav);
+        if (savedInstanceState == null) {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.container, new ChatFragment());
+            ft.commit();
+        }
+//         btnNav.setSelectedItemId(R.id.nav_chat);
+
+
+        btnNav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                 if (item.getItemId()==R.id.nav_chat) {
+                    FragmentManager fm=getSupportFragmentManager();
+                    FragmentTransaction ft=fm.beginTransaction();
+                    ft.add(R.id.container,new ChatFragment());
+                    ft.commit();
+                }
+                else if (item.getItemId()==R.id.nav_Status){
+                    FragmentManager fm=getSupportFragmentManager();
+                    FragmentTransaction ft=fm.beginTransaction();
+                    ft.replace(R.id.container,new StatusFragment());
+                    ft.commit();
+
+                }  else if (item.getItemId()==R.id.nav_calls) {
+                    FragmentManager fm=getSupportFragmentManager();
+                    FragmentTransaction ft=fm.beginTransaction();
+                    ft.replace(R.id.container,new CallsFragment());
+                    ft.commit();
+                }
+//
+            }
+        });
     }
 }
